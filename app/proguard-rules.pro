65841818c028b977018bdeb1454ff07a9413f37a
andrei <pstumon708@gmail.com>
2023-06-23 15:39:18 +0300

    added the page with the payment

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,3 +1,19 @@
+-keepclassmembers class * {
+    @android.webkit.JavascriptInterface <methods>;
+}
+
+-keepattributes JavascriptInterface
+-keepattributes *Annotation*
+
+-dontwarn com.razorpay.**
+-keep class com.razorpay.** {*;}
+
+-optimizations !method/inlining/*
+
+-keepclasseswithmembers class * {
+  public void onPayment*(...);
+}
+
 # Add project specific ProGuard rules here.
 # You can control the set of applied configuration files using the
 # proguardFiles setting in build.gradle.
--- app/src/main/java/com/example/myapp/ItemActivity.kt
+++ app/src/main/java/com/example/myapp/ItemActivity.kt
@@ -1,11 +1,57 @@
 package com.example.myapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.razorpay.Checkout
+import org.json.JSONObject
 
 class ItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item)
+
+        Checkout.preload(applicationContext)
+        val co = Checkout()
+        // apart from setting it in AndroidManifest.xml, keyId can also be set
+        // programmatically during runtime
+        co.setKeyID("rzp_test_zz3JsrvHv17WPq")
+
+        val title: TextView = findViewById(R.id.item_list_title_one)
+        val text: TextView = findViewById(R.id.item_list_text)
+        val btn: TextView = findViewById(R.id.button_buy)
+
+        title.text = intent.getStringExtra("itemTitle")
+        text.text = intent.getStringExtra("itemText")
+        btn.setOnClickListener {
+            startPayment()
+        }
+    }
+    private fun startPayment() {
+        /*
+        *  You need to pass the current activity to let Razorpay create CheckoutActivity
+        * */
+        val activity: Activity = this
+        val co = Checkout()
+
+        try {
+            val options = JSONObject()
+            options.put("name","Razorpay Corp")
+            options.put("description","Demoing Charges")
+            //You can omit the image option to fetch the image from the dashboard
+            options.put("image","https://s3.amazonaws.com/rzp-mobile/images/rzp.jpg")
+            options.put("theme.color", "#3399cc");
+            options.put("currency","INR");
+            options.put("order_id", "order_M5K3KezuOTKERQ");
+            options.put("amount",10000)//pass amount in currency subunits
+
+
+            co.open(activity,options)
+        }catch (e: Exception){
+            Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
     }
 }
